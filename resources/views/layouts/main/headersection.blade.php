8cd7eb23286185ce18bddec4526210e09502be0c
Mehar Hassan <hosterlohosting@gmail.com>
2026-01-13 15:59:37 -0800

    UI: Extensive Mobile Responsiveness Improvements (Typography, Tables, Buttons)

--- resources/views/layouts/main/headersection.blade.php
+++ resources/views/layouts/main/headersection.blade.php
@@ -4,21 +4,23 @@
       <div class="header-body">
         <div class="row align-items-center py-4">
           <div class="col-lg-12 col-12">
-            @isset($prev)
-              <a href="{{ url($prev) }}" class="btn btn-outline-primary btn-sm btn-icon"><i
-                  class="fas fa-arrow-left"></i></a>
-            @endisset
-            <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
-              <ol class="breadcrumb breadcrumb-links">
-                <li class="breadcrumb-item"><a href="{{ url('/login') }}"><i class="fas fa-home"></i></a></li>
-                @if(isset($title))
-                  <li class="breadcrumb-item"><a href="#">{!! $title ?? '' !!}</a></li>
-                @endif
-                @foreach(request()->segments() as $segment)
-                  <li class="breadcrumb-item"><a href="#">{{ Str::limit($segment, 28) }}</a></li>
-                @endforeach
-              </ol>
-            </nav>
+            <div class="d-flex align-items-center flex-wrap">
+              @isset($prev)
+                <a href="{{ url($prev) }}" class="btn btn-outline-primary btn-sm btn-icon mr-2"><i
+                    class="fas fa-arrow-left"></i></a>
+              @endisset
+              <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                <ol class="breadcrumb breadcrumb-links">
+                  <li class="breadcrumb-item"><a href="{{ url('/login') }}"><i class="fas fa-home"></i></a></li>
+                  @if(isset($title))
+                    <li class="breadcrumb-item"><a href="#">{!! $title ?? '' !!}</a></li>
+                  @endif
+                  @foreach(request()->segments() as $segment)
+                    <li class="breadcrumb-item"><a href="#">{{ Str::limit($segment, 28) }}</a></li>
+                  @endforeach
+                </ol>
+              </nav>
+            </div>
 
             <!-- Moved Buttons to Left Side -->
             @isset($buttons)
