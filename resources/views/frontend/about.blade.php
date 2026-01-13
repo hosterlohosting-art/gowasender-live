040283ab0948d0deb3a779218d7279c39c6241d0
Mehar Hassan <hosterlohosting@gmail.com>
2026-01-13 06:47:51 -0800

    Fix: Remove 'Area' section from About page (Green Bar)

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1,10 +1,9 @@
 @extends('frontend.layouts.main')
 @section('content')
-@include('frontend.layouts.header')
- <main>
+   @include('frontend.layouts.header')
+   <main>
       @include('frontend.sections.about')
       @include('frontend.sections.faq')
-      @include('frontend.sections.area')
-   
+
    </main>
 @endsection
