9dabed805c92a2ceb745331b21987149a8fbbea3
Mehar Hassan <hosterlohosting@gmail.com>
2026-01-23 08:25:15 -0800

    Fix return type hints in ChatbotController and final code sweep

--- app/Http/Controllers/User/ChatbotController.php
+++ app/Http/Controllers/User/ChatbotController.php
@@ -19,7 +19,7 @@ class ChatbotController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function index()
     {
@@ -40,7 +40,7 @@ class ChatbotController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
@@ -108,7 +108,7 @@ class ChatbotController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
@@ -166,7 +166,7 @@ class ChatbotController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
